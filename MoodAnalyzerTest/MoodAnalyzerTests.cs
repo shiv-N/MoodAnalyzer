a6d7fdc1e58072840ca477d2e2f0ffc0c8e21c41
Saurabh Navdkar <saurabh.navdkar@gmail.com>
2020-09-11 23:48:23 +0530

    Added: Mood Analyzer Nunit Test Project

---
+++ MoodAnalyzerTest/MoodAnalyzerTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace MoodAnalyzerTest
+{
+    public class MoodAnalyzerTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void GivenSadMoodShouldReturnSAD()
+        {
+            Assert.Pass();
+        }
+    }
+}
